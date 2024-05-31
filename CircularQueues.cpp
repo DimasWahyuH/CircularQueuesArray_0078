086b93f0a52831065068dbc93c8e1fed3248249d
DimasWahyuH <dimaswh19@gmail.com>
2024-05-31 15:11:32 +0700

    Menambahkan else jika FRONT_position > REAR_position dan iterasi dari FRONT hingga akhir array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -80,4 +80,13 @@ void display() {
         }
         cout << endl;
     }
+    else {
+        //Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+        while (FRONT_position <= max -1) {
+            cout << queues_array[FRONT_position] << "   ";
+            FRONT_position++;
+        }
+
+        
+    }
 }
